0616264164b01edc2deebe8ee11ee16cbb28943f
Md Kamrul Hasan <mdkamrulhasanewu@gmail.com>
2019-07-09 18:04:04 +0600

    Add files via upload

---
+++ Student.Information/src/main/java/Application/Repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package Application.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import Application.Entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+}
